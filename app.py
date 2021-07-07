1d2adfd6dc9d533e8d77609b0e4665159cf0f20f
GHEPT <teodoro.edu@hotmail.com>
2021-07-07 20:29:58 -0300

    [ADD] Style

--- app.py
+++ app.py
@@ -64,6 +64,10 @@ class Memes(db.Model):
     self.url2 = new_data.url2
     self.save()
   
+  def delete(self):
+    db.session.delete(self)
+    db.session.commit()
+  
 
 @bp.route('/')
 def home():
@@ -99,7 +103,7 @@ def update(meme_id):
 
   if request.method =='POST':
     form=request.form
-    new_data = Memes(form['modulo'], form['aula'], form['conteudo'], form['texto'], form['url1'], form['url2']) 
+    new_data = Memes(form['modulo'], form['aula'], form['conteudo'], form['texto'], form['url1'], form['url2'])
     meme.update(new_data)
     sucesso = True
   return render_template('update.html', meme=meme,sucesso=sucesso)
